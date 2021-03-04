fb2db5831af36522ccb3485c410136c3311dbffd
dgrammatiko <d.grammatiko@gmail.com>
2021-03-04 11:56:30 +0100

    tests clean up

--- tests/test.js
+++ tests/test.js
@@ -12,7 +12,7 @@ const assert = (input, output, test) => {
     // console.log({input: output})
   } else {
     console.log(`Test ${test} failed ❌`)
-    // console.log({[input]: output})
+    console.log({[input]: output})
   }
 }
 
@@ -21,69 +21,60 @@ const assert = (input, output, test) => {
   /**
    * Associated properties
    */
-  const test1 = await UrlProc.process('body { background-image: url("https://mdn.mozillademos.org/files/16761/star.gif"); }', { from: undefined });
   assert(
     `body { background-image: url("https://mdn.mozillademos.org/files/16761/star.gif${version}"); }`,
-    test1.css,
+    (await UrlProc.process('body { background-image: url("https://mdn.mozillademos.org/files/16761/star.gif"); }', { from: undefined })).css,
     'associated properties #1'
   );
 
-  const test2 = await UrlProc.process("body { list-style-image: url('../images/bullet.jpg'); }", { from: undefined });
   assert(
     `body { list-style-image: url("../images/bullet.jpg${version}"); }`,
-    test2.css,
+    (await UrlProc.process("body { list-style-image: url('../images/bullet.jpg'); }", { from: undefined })).css,
     'associated properties #2'
   );
 
-  const test3 = await UrlProc.process('body { content: url("pdficon.jpg"); }', { from: undefined });
   assert(
     `body { content: url("pdficon.jpg${version}"); }`,
-    test3.css,
+    (await UrlProc.process('body { content: url("pdficon.jpg"); }', { from: undefined })).css,
     'associated properties #3'
   );
 
-  const test4 = await UrlProc.process('body { cursor: url(mycursor.cur); }', { from: undefined });
   assert(
     `body { cursor: url("mycursor.cur${version}"); }`,
-    test4.css,
+    (await UrlProc.process('body { cursor: url(mycursor.cur); }', { from: undefined })).css,
     'associated properties #4'
   );
 
-  const test5 = await UrlProc.process('body { border-image-source: url(/media/diamonds.png); }', { from: undefined });
   assert(
     `body { border-image-source: url("/media/diamonds.png${version}"); }`,
-    test5.css,
+    (await UrlProc.process('body { border-image-source: url(/media/diamonds.png); }', { from: undefined })).css,
     'associated properties #5'
   );
 
-  const test6 = await UrlProc.process("@font-face { font-family: 'Open Sans'; { src: url('fantasticfont.woff'); } }", { from: undefined });
   assert(
     `@font-face { font-family: 'Open Sans'; { src: url("fantasticfont.woff${version}"); } }`,
-    test6.css,
+    (await UrlProc.process("@font-face { font-family: 'Open Sans'; { src: url('fantasticfont.woff'); } }", { from: undefined })).css,
     'associated properties #6'
   );
 
-  const test7 = await UrlProc.process('body { offset-path: url(#path); }', { from: undefined });
   assert(
     `body { offset-path: url(#path); }`,
-    test7.css,
+    (await UrlProc.process('body { offset-path: url(#path); }', { from: undefined })).css,
     'associated properties #7'
   );
 
-  const test8 = await UrlProc.process('body { mask-image: url("masks.svg#mask1"); }', { from: undefined });
   assert(
     `body { mask-image: url("masks.svg${version}#mask1"); }`,
-    test8.css,
+    (await UrlProc.process('body { mask-image: url("masks.svg#mask1"); }', { from: undefined })).css,
     'associated properties #8'
   );
 
   /**
    * Properties with fallbacks
    */
-  const test9 = await UrlProc.process('body { cursor: url(pointer.cur), pointer; }', { from: undefined });
   assert(
     `body { cursor: url("pointer.cur${version}"), pointer; }`,
-    test9.css,
+    (await UrlProc.process('body { cursor: url(pointer.cur), pointer; }', { from: undefined })).css,
     'Properties with fallbacks #9'
   );
 
@@ -91,68 +82,60 @@ const assert = (input, output, test) => {
   /**
    * Associated short-hand properties
    */
-  const test10 = await UrlProc.process('body { background: url("https://mdn.mozillademos.org/files/16761/star.gif") bottom right repeat-x blue; }', { from: undefined });
   assert(
     `body { background: url("https://mdn.mozillademos.org/files/16761/star.gif${version}") bottom right repeat-x blue; }`,
-    test10.css,
+    (await UrlProc.process('body { background: url("https://mdn.mozillademos.org/files/16761/star.gif") bottom right repeat-x blue; }', { from: undefined })).css,
     'Associated short-hand properties #10'
   );
 
-  const test11 = await UrlProc.process('body { border-image: url("/media/diamonds.png") 30 fill / 30px / 30px space; }', { from: undefined });
   assert(
     `body { border-image: url("/media/diamonds.png${version}") 30 fill / 30px / 30px space; }`,
-    test11.css,
+    (await UrlProc.process('body { border-image: url("/media/diamonds.png") 30 fill / 30px / 30px space; }', { from: undefined })).css,
     'Associated short-hand properties #11'
   );
 
   /**
    * As a parameter in another CSS function
    */
-  const test12 = await UrlProc.process('body { background-image: cross-fade(20% url(first.png), url(second.png)); }', { from: undefined });
   assert(
     `body { background-image: cross-fade(20% url("first.png${version}"), url("second.png${version}")); }`,
-    test12.css,
+    (await UrlProc.process('body { background-image: cross-fade(20% url(first.png), url(second.png)); }', { from: undefined })).css,
     'As a parameter in another CSS function #12'
   );
 
-  const test13 = await UrlProc.process('body { mask-image: image(url(mask.png), skyblue, linear-gradient(rgba(0, 0, 0, 1.0), transparent)); }', { from: undefined });
   assert(
     `body { mask-image: image(url("mask.png${version}"), skyblue, linear-gradient(rgba(0, 0, 0, 1.0), transparent)); }`,
-    test13.css,
+    (await UrlProc.process('body { mask-image: image(url(mask.png), skyblue, linear-gradient(rgba(0, 0, 0, 1.0), transparent)); }', { from: undefined })).css,
     'As a parameter in another CSS function #13'
   );
 
   /**
    * As part of a non-shorthand multiple value
    */
-  const test14 = await UrlProc.process('body { content: url(star.svg) url(star.svg) url(star.svg) url(star.svg) url(star.svg); }', { from: undefined });
   assert(
     `body { content: url("star.svg${version}") url("star.svg${version}") url("star.svg${version}") url("star.svg${version}") url("star.svg${version}"); }`,
-    test14.css,
+    (await UrlProc.process('body { content: url(star.svg) url(star.svg) url(star.svg) url(star.svg) url(star.svg); }', { from: undefined })).css,
     'As part of a non-shorthand multiple value #14'
   );
 
   /**
    * At-rules
    */
-  const test15 = await UrlProc.process('@document url("https://www.example.com/")', { from: undefined });
   assert(
     `@document url("https://www.example.com/${version}")`,
-    test15.css,
+    (await UrlProc.process('@document url("https://www.example.com/")', { from: undefined })).css,
     'At-rules #15'
   );
 
-  const test16 = await UrlProc.process('@import url("https://www.example.com/style.css");', { from: undefined });
   assert(
     `@import url("https://www.example.com/style.css${version}");`,
-    test16.css,
+    (await UrlProc.process('@import url("https://www.example.com/style.css");', { from: undefined })).css,
     'At-rules #16'
   );
 
-  const test17 = await UrlProc.process('@namespace url(http://www.w3.org/1999/xhtml)', { from: undefined });
   assert(
     `@namespace url("http://www.w3.org/1999/xhtml${version}")`,
-    test17.css,
+    (await UrlProc.process('@namespace url(http://www.w3.org/1999/xhtml)', { from: undefined })).css,
     'At-rules #17'
   );
 })();
